2816c75d67d050ba84699a1f122b528d95550128
Sergey Fomin <fomin-sm1998@mail.ru>
2024-02-17 15:26:57 +0300

    models added: Country, Manufacturer, Car, Comment

---
+++ test_project/myapp/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+from django.db import models
+
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Car(models.Model):
+    name = models.CharField(max_length=100)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    start_year = models.IntegerField()
+    end_year = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Comment(models.Model):
+    email = models.EmailField()
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+
+    def __str__(self):
+        return f"{self.car.name} - {self.text[:20]}..."
