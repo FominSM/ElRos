c742ef3de96ddf31a37119354f5f0a5a0077c958
FominS <fomin-sm1998@mail.ru>
2024-02-22 13:34:43 +0300

    routes from task point 3 are arranged through a router

--- test_project/myapp/urls.py
+++ test_project/myapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import DefaultRouter, SimpleRouter
 from .views import *
 from .exported import *
 
@@ -7,38 +7,40 @@ from .exported import *
 router_api = DefaultRouter()
 
 # регистрация марщрутов для каждого представления в views.py
-router_api.register(r'countries', CountryViewSet)
-router_api.register(r'manufacturers', ManufacturerViewSet)
-router_api.register(r'cars', CarViewSet)
-router_api.register(r'comments', CommentViewSet)
-
-# for def export_to_xlsx()
-# http://127.0.0.1:8000/api/export_to_xlsx/country/
-# http://127.0.0.1:8000/api/export_to_xlsx/manufacturer/
-# http://127.0.0.1:8000/api/export_to_xlsx/car/
-# http://127.0.0.1:8000/api/export_to_xlsx/comment/
-
-# for def export_to_csv()
-# http://127.0.0.1:8000/api/export_to_csv/country/
-# http://127.0.0.1:8000/api/export_to_csv/manufacturer/
-# http://127.0.0.1:8000/api/export_to_csv/car/
-# http://127.0.0.1:8000/api/export_to_csv/comment/
+router_api.register(r'country', CountryViewSet)
+router_api.register(r'manufacturer', ManufacturerViewSet)
+router_api.register(r'car', CarViewSet)
+router_api.register(r'comment', CommentViewSet)
 
 
-# router_api_v3 = DefaultRouter()
+'''
+Обычно вам не нужно указывать аргумент basename, но если у вас есть набор представлений, в котором вы определили 
+пользовательский метод get_queryset, то набор представлений может не иметь атрибута .queryset. Если вы попытаетесь 
+зарегистрировать этот набор представлений, вы увидите ошибку
+'''
+router_api_v3 = DefaultRouter()
+router_api_v3.register(r'country', CountryViewSetV3, basename='Country')
+router_api_v3.register(r'manufacturer', ManufacturerViewSetV3, basename='Manufacturer')
+router_api_v3.register(r'car', CarViewSetV3, basename='Car')
 
-# router_api_v3.register(r'countries', CountryViewSetV3)
 
+'''
+url example for def export_to_xlsx()
+http://127.0.0.1:8000/api/export_to_xlsx/country/
+http://127.0.0.1:8000/api/export_to_xlsx/manufacturer/
+http://127.0.0.1:8000/api/export_to_xlsx/car/
+http://127.0.0.1:8000/api/export_to_xlsx/comment/
+
+url example for def export_to_csv()
+http://127.0.0.1:8000/api/export_to_csv/country/
+http://127.0.0.1:8000/api/export_to_csv/manufacturer/
+http://127.0.0.1:8000/api/export_to_csv/car/
+http://127.0.0.1:8000/api/export_to_csv/comment/
+'''
 
 urlpatterns = [
-    path('', include(router_api.urls)),
-
+    path('v1/', include(router_api.urls)),
+    path('v2/', include(router_api_v3.urls)),
     path('export_to_xlsx/<str:data_model>/', export_to_xlsx, name='export_to_xlsx'),
     path('export_to_csv/<str:data_model>/', export_to_csv, name='export_to_csv'),
-
-    path('v3/country/<int:pk>/', CountryViewSetV3.as_view({'get': 'retrieve'}), name='get_country_with_parameters'),
-    path('v3/manufacturer/<int:pk>/', ManufacturerViewSetV3.as_view({'get': 'retrieve'}), name='get_manufacturer_with_parameters'),
-    path('v3/car/<int:pk>/', CarViewSetV3.as_view({'get': 'retrieve'}), name='get_car_with_parameters'),
-
-    # path('v3/', include(router_api_v3.urls)),
 ]
