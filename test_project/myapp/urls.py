5fa2378ec21d4f24913a9140988e7866774d651b
Sergey Fomin <fomin-sm1998@mail.ru>
2024-03-03 23:14:40 +0300

    routes corrected

--- test_project/myapp/urls.py
+++ test_project/myapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path, re_path
 from rest_framework.routers import DefaultRouter
-from .views import *
-from .exported import *
+from .views import CountryViewSet, ManufacturerViewSet, CarViewSet, CommentViewSet
+
 
 
 router_api_v1 = DefaultRouter()
@@ -11,26 +11,8 @@ router_api_v1.register(r'manufacturer', ManufacturerViewSet)
 router_api_v1.register(r'car', CarViewSet)
 router_api_v1.register(r'comment', CommentViewSet)
 
-
-'''
-Обычно вам не нужно указывать аргумент basename, но если у вас есть набор представлений, в котором вы определили 
-пользовательский метод get_queryset, то набор представлений может не иметь атрибута .queryset. Если вы попытаетесь 
-зарегистрировать этот набор представлений, вы увидите ошибку
-'''
-# router_api_v2 = DefaultRouter()
-# router_api_v2.register(r'country', CountryViewSetV3, basename='Country')
-# router_api_v2.register(r'manufacturer', ManufacturerViewSetV3, basename='Manufacturer')
-# router_api_v2.register(r'car', CarViewSetV3, basename='Car')
-
-
-
-
 urlpatterns = [
     path('v1/', include(router_api_v1.urls)),
-    # path('v2/', include(router_api_v2.urls)),
-
-
-    # Список актуальных пользователей доступен только через передачу токена
     path('djoser/auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
 ]
