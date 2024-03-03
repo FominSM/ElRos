3c459fd4849b10366a68538809acb6910b27ffc9
Sergey Fomin <fomin-sm1998@mail.ru>
2024-03-03 23:18:14 +0300

    final edits

--- test_project/myapp/views.py
+++ test_project/myapp/views.py
@@ -1,12 +1,9 @@
-from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from .models import Country, Manufacturer, Car, Comment
-from .serializers import *
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, BasePermission
+from .serializers import CountrySerializer, ManufacturerSerializer, CarSerializer, CommentSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, BasePermission
 from rest_framework.authentication import TokenAuthentication
 from .exported import ExportDataToCsvOrXlsx
-from django.forms.models import model_to_dict
 
 
 
@@ -15,7 +12,9 @@ class BaseCustomModelViewSet(viewsets.ModelViewSet):
         attr_to_export = ('id')
 
         if 'get' in request.GET:
-            return ExportDataToCsvOrXlsx(request, self.queryset, self.attr_to_export, request.GET['get']).get_file()
+            file_export = ExportDataToCsvOrXlsx(request, self.queryset, self.attr_to_export, request.GET['get'])
+
+            return file_export.get_file()
 
         return super(BaseCustomModelViewSet, self).list(request, *args, **kwargs) 
   
@@ -24,24 +23,24 @@ class CountryViewSet(BaseCustomModelViewSet):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
     attr_to_export = ('id', 'name')
-    # authentication_classes = (TokenAuthentication, )
-    # permission_classes = (IsAuthenticatedOrReadOnly, )
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticatedOrReadOnly, )
 
 
 class ManufacturerViewSet(BaseCustomModelViewSet):
     queryset = Manufacturer.objects.all()
     serializer_class = ManufacturerSerializer
     attr_to_export = ('id', 'name', 'country')
-    # authentication_classes = (TokenAuthentication, )
-    # permission_classes = (IsAuthenticatedOrReadOnly, )
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticatedOrReadOnly, )
     
 
 class CarViewSet(BaseCustomModelViewSet):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
     attr_to_export = ('id', 'name', 'manufacturer', 'start_year', 'end_year')
-    # authentication_classes = (TokenAuthentication, )
-    # permission_classes = (IsAuthenticatedOrReadOnly, )
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticatedOrReadOnly, )
    
 
 class _ReadOnlyAndAppend(BasePermission):
@@ -53,37 +52,5 @@ class CommentViewSet(BaseCustomModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     attr_to_export = ('id', 'created_at', 'email', 'car', 'text')
-    # authentication_classes = (TokenAuthentication, )
-    # permission_classes = (IsAuthenticatedOrReadOnly|_ReadOnlyAndAppend, )
-
-
-
-
-
-
-
-# class CountryViewSetV3(viewsets.ViewSet):
-#     # переопределяем метод для выбора обьекта по указанному в url id
-#     def retrieve(self, request, pk=None):
-#         # получаем всю коллекцию класса
-#         queryset = Country.objects.all()
-#         # выбираем конкретный объект по id
-#         country = get_object_or_404(queryset, pk=pk)
-#         # передаем объект сериализатору
-#         serializer = CountrySerializerV3(country)
-#         # отправляем клиенту данные в b'data'
-#         return Response(serializer.data)
-    
-# class ManufacturerViewSetV3(viewsets.ViewSet):
-#     def retrieve(self, request, pk=None):
-#         queryset = Manufacturer.objects.all()
-#         manufacturer = get_object_or_404(queryset, pk=pk)
-#         serializer = ManufacturerSerializerV3(manufacturer)
-#         return Response(serializer.data)
-    
-# class CarViewSetV3(viewsets.ViewSet):
-#     def retrieve(self, request, pk=None):
-#         queryset = Car.objects.all()
-#         car = get_object_or_404(queryset, pk=pk)
-#         serializer = CarSerializerV3(car)
-#         return Response(serializer.data)
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticatedOrReadOnly|_ReadOnlyAndAppend, )
